ac88c53e22a8f1fe00e3db762b48724fd4f19869
Joe Hewitt <joe@joehewitt.com>
2011-06-28 11:54:44 -0500

    * Generate pages with scripts included or loaded separately * Generate javascripts with dependencies loaded asynchronously * Basic web server functions

--- test/appjs-test.js
+++ test/appjs-test.js
@@ -1,8 +1,10 @@
 var path = require('path'),
     assert = require('assert'),
-    vows = require('vows');
+    vows = require('vows'),
+    _ = require('underscore');
 
 require.paths.unshift(path.join(__dirname, '..', 'lib'));
+require.paths.unshift(path.join(__dirname, 'modules'));
 
 var appjs = require('appjs');
 
@@ -11,8 +13,7 @@ var appjs = require('appjs');
 vows.describe('appjs basics').addBatch({
     'module': {
         topic: function() {
-        	var modulesPath = path.normalize(path.join(__dirname, 'modules'));
-        	var loader = new appjs.Loader([modulesPath]);
+        	var loader = new appjs.Loader();
         	appjs.loadApp("testmod1", loader, this.callback);
 	    },
 
@@ -22,11 +23,55 @@ vows.describe('appjs basics').addBatch({
         	},
 
 	        'that include': function(deps) {
-    			assert.deepEqual(deps, [
+    			assert.deepEqual(_.keys(deps), [
     				'testmod1',
+    				'testmod1/lib/dep2',
     				'testmod1/lib/dep1',
 	    		]);
         	},
+        },
+
+        'renders': {
+        	topic: function(app) {
+        		var maker = new appjs.PageMaker(app, 'http://localhost:8080/', '/');
+        		maker.make(this.callback);
+        	},
+
+	        'as page': function(html) {
+    			assert.equal(html,
+	    			'<!DOCTYPE html>'+
+	    			'<html app="testmod1">'+
+	    			'<head>'+
+	    			'<base href="/"/>'+
+	    			'</head>'+
+		    		'<body></body>'+
+			    	'</html>');
+        	},
+        },
+
+        'renders compressed': {
+        	topic: function(app) {
+        		var maker = new appjs.PageMaker(app, 'http://localhost:8080/', '/', true, true);
+        		maker.make(this.callback);
+        	},
+
+	        'a page': function(html) {
+    			assert.equal(html,
+	    			'<!DOCTYPE html>'+
+	    			'<html app="testmod1">'+
+	    			'<head>'+
+	    			'<base href="/"/>'+
+	    			'<script type="appjs/cached" id="appjs/js/testmod1">'+
+	    			'define(["./dep1","./dep2"],function(a,b){var c=a,d=b})</script>\n'+
+	    			'<script type="appjs/cached" id="appjs/js/testmod1/lib/dep2">'+
+	    			'define([],function(){})</script>\n'+
+	    			'<script type="appjs/cached" id="appjs/js/testmod1/lib/dep1">'+
+	    			'define([],function(){var a=1})</script>'+
+	    			'</head>'+
+		    		'<body></body>'+
+			    	'</html>');
+        	},
         }
+
     },     
 }).export(module);
--- test/modules/testmod1/lib/testmod1.js
+++ test/modules/testmod1/lib/testmod1.js
@@ -1,1 +1,2 @@
 var dep1 = require('./dep1');
+var dep2 = require('./dep2');
